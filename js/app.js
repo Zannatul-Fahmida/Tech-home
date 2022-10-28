d0f3e77431ab111cc7f2ff813de8614f44b64fd2
zannatul <jannatulfahmida15@gmail.com>
2022-10-28 17:20:16 +0600

    some issues fixed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+const homeContainer = document.getElementById("home-container");
+const questionContainer = document.getElementById("question-container");
+const resultContainer = document.getElementById("result-container");
+const timeoutContainer = document.getElementById("timeout-container");
 const loadQuestions = () => {
-  const homeContainer = document.getElementById("home-container");
-  const questionContainer = document.getElementById("question-container");
   homeContainer.style.display = "none";
   questionContainer.style.display = "block";
   const url = "../questions.json";
@@ -11,16 +13,16 @@ const loadQuestions = () => {
 };
 const showQuestions = (ques) => {
   const allQues = ques.map((qs) => qs);
-  let count = 1;
-  for (const question of allQues) {
+  for (let i = 0; i < allQues.length; i++) {
     const div = document.createElement("div");
     div.classList.add("ques-container");
     div.innerHTML = `<div class="ques">
-      <h3>${count++}. ${question.title}</h3>
-      ${question.options.map(
+      <h3>${i + 1}. ${allQues[i].title}</h3>
+      ${allQues[i].options.map(
         (opt) => `
-      <input id=${opt} type="radio" name="option" value=${opt}>
-      <label for=${opt}>${opt}</label> <br>`
+      <input class="options" data-Index="${i}" id=${opt} type="radio" name='option${i}' value=${opt}>
+      <label for=${opt}>${opt}</label> <br>
+      `
       )}
       </div>
       `;
@@ -28,62 +30,50 @@ const showQuestions = (ques) => {
   }
 };
 const showTimer = () => {
-  var time_in_minutes = 10;
-  var current_time = Date.parse(new Date());
-  var deadline = new Date(current_time + time_in_minutes * 60 * 1000);
+  let timeInMinutes = 10;
+  let currentTime = Date.parse(new Date());
+  let deadline = new Date(currentTime + timeInMinutes * 60 * 1000);
 
-  function time_remaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  function timeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date());
+    let seconds = Math.floor((t / 1000) % 60);
+    let minutes = Math.floor((t / 1000 / 60) % 60);
     return {
       total: t,
-      days: days,
-      hours: hours,
       minutes: minutes,
       seconds: seconds,
     };
   }
-  function run_clock(id, endtime) {
-    var clock = document.getElementById(id);
-    function update_clock() {
-      var t = time_remaining(endtime);
+  function runClock(id, endtime) {
+    let clock = document.getElementById(id);
+    function updateClock() {
+      let t = timeRemaining(endtime);
       clock.innerHTML = "00: " + t.minutes + ": " + t.seconds;
       if (t.total <= 0) {
         clearInterval(timeinterval);
         timeUp();
       }
     }
-    update_clock(); // run function once at first to avoid delay
-    var timeinterval = setInterval(update_clock, 1000);
+    updateClock(); 
+    let timeinterval = setInterval(updateClock, 1000);
   }
-  run_clock("timer-container", deadline);
+  runClock("timer-container", deadline);
 };
 const timeUp = () => {
-  const homeContainer = document.getElementById("home-container");
-  const questionContainer = document.getElementById("question-container");
-  const resultContainer = document.getElementById("result-container");
-  const timeoutContainer = document.getElementById("timeout-container");
   homeContainer.style.display = "none";
   questionContainer.style.display = "none";
   resultContainer.style.display = "none";
   timeoutContainer.style.display = "block";
 };
-const getResult = () => {
-  const homeContainer = document.getElementById("home-container");
-  const questionContainer = document.getElementById("question-container");
-  const resultContainer = document.getElementById("result-container");
+const getResult = (selectedAns) => {
   homeContainer.style.display = "none";
   questionContainer.style.display = "none";
   resultContainer.style.display = "block";
+  let userSolution = selectedAns.value;
 };
 const startAgain = () => {
-  const homeContainer = document.getElementById("home-container");
-  const questionContainer = document.getElementById("question-container");
-  const resultContainer = document.getElementById("result-container");
   homeContainer.style.display = "flex";
   questionContainer.style.display = "none";
   resultContainer.style.display = "none";
+  location.reload();
 };
